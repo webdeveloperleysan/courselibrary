494587ef9d82cb3fa756190344bab7d0b1076039
Leysan Magnus <leysan.web.developer@gmail.com>
2023-09-28 18:42:05 +0200

    books presentation layer: getting list and display on UI

---
+++ src/main/java/com/application/courselibrary/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.application.courselibrary.controller;
+
+import com.application.courselibrary.entity.Book;
+import com.application.courselibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/books")
+    public String findAllBooks(Model model) {
+        List<Book> books = bookService.findAllBooks();
+        model.addAttribute("books", books);
+        return "books";
+    }
+}
