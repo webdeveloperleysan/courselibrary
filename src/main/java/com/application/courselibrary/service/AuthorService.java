b092a5c52b9de4ae2241eac0073ba27d852fb538
Leysan Magnus <leysan.web.developer@gmail.com>
2023-09-28 14:35:36 +0200

    data access and service layer added for Author

---
+++ src/main/java/com/application/courselibrary/service/AuthorService.java
@@ -0,0 +1,38 @@
+package com.application.courselibrary.service;
+
+import com.application.courselibrary.entity.Author;
+import com.application.courselibrary.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorService {
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Author> findAllAuthors (){
+        return authorRepository.findAll();
+    }
+
+    public Author findAuthorById (Long id){
+        return authorRepository.findById(id).orElseThrow(()-> new RuntimeException("Author not found"));
+    }
+
+    public void createNewAuthor (Author author){
+        authorRepository.save(author);
+    }
+
+    public void updateAuthor(Author author){
+        authorRepository.save(author);
+
+    }
+
+    public void deleteAuthorById (Long id){
+        Author author= authorRepository.findById(id).orElseThrow(()->new RuntimeException("Author not found"));
+        authorRepository.deleteById(author.getId());
+    }
+
+
+}
